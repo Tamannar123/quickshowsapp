7779118b95261198567e97f0f1ee37137ce3dbad
Tamannar123 <tamannarana73@gmail.com>
2025-10-06 15:26:30 +0530

    change keys

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     try{
         mongoose.connection.on('connected',()=>console.log('database connected'));
-await mongoose.connect(`${process.env.ATLAS_URI}`)
+await mongoose.connect(`${process.env.MONGODB_URI}`)
     }catch(error){
    console.log(error.message)
     }
