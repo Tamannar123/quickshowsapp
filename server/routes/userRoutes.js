f41644adbfa7e5acef7ab93965e657fba894242a
Tamannar123 <tamannarana73@gmail.com>
2025-10-08 12:49:49 +0530

    add

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserBookings } from '../controllers/bookingController.js';
+import {getUserBookings} from '../controllers/bookingController.js'
 import { updateFavorite, getFavorite } from '../controllers/favoriteController.js';
 
 
